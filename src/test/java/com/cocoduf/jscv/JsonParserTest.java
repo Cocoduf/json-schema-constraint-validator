8023f0c4ecdddd2eaac93d6aa61bcefc8f8aafd5
cocoduf <pikopoki86@yahoo.fr>
2017-05-14 16:26:13 -0400

    :white_check_mark: Test loop over data.json entry set

--- src/test/java/com/cocoduf/jscv/JsonParserTest.java
+++ src/test/java/com/cocoduf/jscv/JsonParserTest.java
@@ -1,36 +1,55 @@
 package com.cocoduf.jscv;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by cocoduf on 17-05-11.
  */
 public class JsonParserTest {
 
-    @Test
-    public void testGsonExample() {
+    private File getFileResource(String fileName) {
+        return new File(getClass().getClassLoader().getResource(fileName).getFile());
+    }
+
+    private JsonObject getJsonObject(String fileName) {
         JsonParser parser = new JsonParser();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data.json");
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
         Reader reader = new InputStreamReader(inputStream);
-        JsonObject rootObject = parser.parse(reader).getAsJsonObject();
-        Assert.assertEquals(rootObject.get("price").getAsInt(), 750);
+        return parser.parse(reader).getAsJsonObject();
     }
 
     @Test
-    public void testValidatingResources() {
-
-        File schemaFile = new File(getClass().getClassLoader().getResource("constraints.json").getFile());
-        File jsonFile = new File(getClass().getClassLoader().getResource("data.json").getFile());
-
+    public void testJsonSchemaValidator() {
         try {
+            File schemaFile = getFileResource("constraints.json");
+            File jsonFile = getFileResource("data.json");
             Assert.assertEquals(true, ValidationUtils.isJsonValid(schemaFile, jsonFile));
         } catch (Exception e) {
-            Assert.assertEquals(1,2);
+            Assert.assertEquals(true,false);
+        }
+    }
+
+    @Test
+    public void testLoadJsonResources() {
+        JsonObject dataRootObject = getJsonObject("data.json");
+        JsonObject constraintsRootObject = getJsonObject("constraints.json");
+    }
+
+    @Test
+    public void testLoopOverDataEntrySet() {
+        JsonObject rootObject = getJsonObject("data.json");
+        Set<Map.Entry<String, JsonElement>> entrySet = rootObject.entrySet();
+        Assert.assertEquals(entrySet.size(), 3);
+        for (Map.Entry<String, JsonElement> entry : entrySet) {
+            Assert.assertNotEquals(entry.getValue(), null);
         }
     }
 }
